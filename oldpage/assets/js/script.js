c3e2c7611ba1e9135e2511d316f64afa12f0c8ef
FairGames <fairgamesnetwork@gmail.com>
2022-05-10 18:52:50 +0200

    Add files via upload

--- oldpage/assets/js/script.js
+++ oldpage/assets/js/script.js
@@ -62,7 +62,7 @@ function startMainTyping() {
 }
 
 function switchScreen() {
-  document.title = 'Dev86.tk | home';
+  document.title = 'dev86.tk | home';
 
   $('.intro').fadeOut(1000, function () {
     $('.bg-image').fadeIn(1000);
